55ff66ceb667c18ae6b9370b1790a3d6fb0927d5
priyapawar264 <46129620+priyapawar264@users.noreply.github.com>
2019-01-03 17:50:44 +0530

    Update cprogram2.c

--- cprogram2.c
+++ cprogram2.c
@@ -8,11 +8,11 @@ int main(int argc,char* argv[])
     int counter;
     printf("Program Name Is: %s",argv[0]);
     if(argc==1)
-        printf("\nNo Extra Command Line Argument Passed Other Than Program Name");
+        printf("\nNo Extra Command Line Argument Passed Other Than Program Name\n");
     if(argc>=2)
     {
         printf("\nNumber Of Arguments Passed: %d",argc);
-        printf("\n----Following Are The Command Line Arguments Passed----");
+        printf("\n----Following Are The Command Line Arguments Passed----\n");
         for(counter=0;counter<argc;counter++)
             printf("\nargv[%d]: %s",counter,argv[counter]);
     }
