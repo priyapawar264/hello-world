2729ea7d7eadda755c46d26754257b403b985467
priyapawar264 <46129620+priyapawar264@users.noreply.github.com>
2019-01-04 11:46:37 +0530

    Update cprogram2.c

--- cprogram2.c
+++ cprogram2.c
@@ -15,9 +15,7 @@ int main(int argc,char* argv[])
         printf("\nNumber Of Arguments Passed: %d",argc);
         printf("\n----Following Are The Command Line Arguments Passed----");
         for(counter=0;counter<argc;counter++)
-            printf("\natoi(argv[counter]): %s",argv[counter]);
-            sum = sum + atoi(argv[counter]);
+            printf("\natoi(argv[%d]): %s",counter,argv[counter]);
     }
-    printf("The sum is: %d", sum);
     return 0;
 }
