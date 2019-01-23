d9b96e9208f598dcb30a22657edc9afd1c2fd421
priyapawar264 <46129620+priyapawar264@users.noreply.github.com>
2019-01-23 15:59:39 +0530

    Update cprogram.c

--- cprogram.c
+++ cprogram.c
@@ -6,18 +6,18 @@ int main(int argc,char* argv[])
 {
     int counter;
     int sum;
-    printf("Program Name Is: %s",argv[0]);
+    printf("\nProgram Name Is: %s",argv[0]);
     if(argc>=1)
     {
         printf("\nNumber Of Arguments Passed are: %d",argc);
         for(counter=1;counter<argc;counter++)
             if(atoi(argv[counter]) != 10)
             {
-                printf("***********Test execution success**********");
+                printf("\n***********Test execution success**********");
             }
             else
             {
-                printf("***********Failure is occured**********");
+                printf("\n***********Failure is occured**********");
             }
     }
     return 0;
