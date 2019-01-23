1dd7646cdb4c4d6a3a813253cfa6b8fbac7aab2b
priyapawar264 <46129620+priyapawar264@users.noreply.github.com>
2019-01-23 14:22:07 +0530

    Update cprogram.c

--- cprogram.c
+++ cprogram.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
+#include <netdb.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <arpa/inet.h>
 
 int main(int argc,char* argv[])
 {
