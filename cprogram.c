97b02e6fb98ba862265c88ad8e26726edfceeca0
priyapawar264 <46129620+priyapawar264@users.noreply.github.com>
2019-01-23 15:54:50 +0530

    Update cprogram.c

--- cprogram.c
+++ cprogram.c
@@ -7,14 +7,18 @@ int main(int argc,char* argv[])
     int counter;
     int sum;
     printf("Program Name Is: %s",argv[0]);
-    if(argc==1)
-        printf("\nNo Extra Command Line Argument Passed Other Than Program Name\n");
     if(argc>=2)
     {
         printf("\nNumber Of Arguments Passed are: %d",argc);
-        printf("\n----Following Are The Command Line Arguments Passed----");
-        for(counter=0;counter<argc;counter++)
-            printf("\natoi(argv[%d]): %s",counter,argv[counter]);
+        for(counter=1;counter<argc;counter++)
+            if(atoi(argv[counter]) != 10)
+            {
+                printf("***********Test execution success**********");
+            }
+            else
+            {
+                printf("***********Failure is occured**********");
+            }
     }
     return 0;
 }
