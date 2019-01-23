1195594f516028e4a8e8dd6de76ac6251c8064f2
priyapawar264 <46129620+priyapawar264@users.noreply.github.com>
2019-01-23 15:56:54 +0530

    Update cprogram.c

--- cprogram.c
+++ cprogram.c
@@ -7,7 +7,7 @@ int main(int argc,char* argv[])
     int counter;
     int sum;
     printf("Program Name Is: %s",argv[0]);
-    if(argc>=2)
+    if(argc>=1)
     {
         printf("\nNumber Of Arguments Passed are: %d",argc);
         for(counter=1;counter<argc;counter++)
